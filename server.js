0bf44fa6003d60b281b1662e8dfba9616a2c72da
zaa92reer@gmail.com <mohammad@Moh_Zar.localdomain>
2021-04-05 13:38:20 +0300

    repo configured

--- server.js
+++ server.js
@@ -3,75 +3,97 @@
 // Application Dependencies
 const express = require('express');
 const superagent = require('superagent');
+const pg = require('pg');
+const cors = require('cors');
 
 // Application Setup
 const app = express();
 const PORT = process.env.PORT || 3000;
+const DATABASE_URL = process.env.DATABASE_URL;
+app.use(cors());
 
-// tell server that all stylesheets inside public 
-app.use(express.static(__dirname + '/public'));
+// Database Connection Setup
+const client = new pg.Client({
+    connectionString: DATABASE_URL,
+    // ssl: {
+    //     rejectUnauthorized: false
+    // }
+});
 
-// Application Middleware
-app.use(express.urlencoded({ extended: true }));
 
-// Set the view engine for server-side templating
-app.set('view engine', 'ejs');
 
-// API Routes
-// Renders the home page
-app.get('/', renderHomePage);
+// // tell server that all stylesheets inside public 
+// app.use(express.static(__dirname + '/public'));
 
-// Renders the search form
-app.get('/searches/new', showForm);
+// // Application Middleware
+// app.use(express.urlencoded({ extended: true }));
 
-// Creates a new search to the Google Books API
-app.post('/searches', createSearch);
+// // Set the view engine for server-side templating
+// app.set('view engine', 'ejs');
 
-// Catch-all
-app.get('*', (request, response) => response.status(404).send('This route does not exist'));
+// // API Routes
+// // Renders the home page
+// app.get('/', renderHomePage);
 
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+// // Renders the search form
+// app.get('/searches/new', showForm);
 
-// Constructor
-function Book(info) {
-    this.image = info.imageLinks.smallThumbnail || 'https://i.imgur.com/J5LVHEL.jpeg';
-    this.title = info.title || 'No title available';
-    this.author = info.authors || ' ';
-    this.description = info.description || ' ';
-}
+// // Creates a new search to the Google Books API
+// app.post('/searches', createSearch);
 
-// Note that .ejs file extension is not required
-function renderHomePage(request, response) {
-    response.render('pages/index');
-}
+// // Catch-all
+// app.get('*', (request, response) => response.status(404).send('This route does not exist'));
 
-function showForm(request, response) {
-    response.render('pages/searches/new');
-}
+// app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
-// No API key required
-function createSearch(request, response) {
-    const url = 'https://www.googleapis.com/books/v1/volumes';
+// // Constructor
+// function Book(info) {
+//     this.image = info.imageLinks.smallThumbnail || 'https://i.imgur.com/J5LVHEL.jpeg';
+//     this.title = info.title || 'No title available';
+//     this.author = info.authors || ' ';
+//     this.description = info.description || ' ';
+// }
 
-    // add the search query to the URL
-    const searchBy = request.body.searchBy;
-    const searchValue = request.body.search;
-    const queryObj = {};
-    if (searchBy === 'title') {
-        queryObj['q'] = `intitle:${searchValue}`;
+// // Note that .ejs file extension is not required
+// function renderHomePage(request, response) {
+//     response.render('pages/index');
+// }
 
-    } else if (searchBy === 'author') {
-        queryObj['q'] = `inauthor:${searchValue}`;
-    }
+// function showForm(request, response) {
+//     response.render('pages/searches/new');
+// }
 
-    // send the URL to the servers API
-    superagent.get(url).query(queryObj).then(apiResponse => {
-        return apiResponse.body.items.map(bookResult => {
-            return new Book(bookResult.volumeInfo);
-        });
-    }).then(results => {
-        response.render('pages/searches/show', { searchResults: results })
-    }).catch((error) => {
-        response.status(500).render('pages/error');
-    });;
-}
+// // No API key required
+// function createSearch(request, response) {
+//     const url = 'https://www.googleapis.com/books/v1/volumes';
+
+//     // add the search query to the URL
+//     const searchBy = request.body.searchBy;
+//     const searchValue = request.body.search;
+//     const queryObj = {};
+//     if (searchBy === 'title') {
+//         queryObj['q'] = `intitle:${searchValue}`;
+
+//     } else if (searchBy === 'author') {
+//         queryObj['q'] = `inauthor:${searchValue}`;
+//     }
+
+//     // send the URL to the servers API
+//     superagent.get(url).query(queryObj).then(apiResponse => {
+//         return apiResponse.body.items.map(bookResult => {
+//             return new Book(bookResult.volumeInfo);
+//         });
+//     }).then(results => {
+//         response.render('pages/searches/show', { searchResults: results })
+//     }).catch((error) => {
+//         response.status(500).render('pages/error');
+//     });;
+// }
+
+// Connect to DB and Start the Web Server
+client.connect().then(() => {
+    app.listen(PORT, () => {
+        console.log("Connected to database:", client.connectionParameters.database)
+        console.log('Server up on', PORT);
+    });
+})
