440eeb1f79208d007b10ff0864a945c32885ad12
zaa92reer@gmail.com <mohammad@Moh_Zar.localdomain>
2021-04-06 17:00:18 +0300

    lab13 done

--- server.js
+++ server.js
@@ -5,93 +5,89 @@ require('dotenv').config();
 const express = require('express');
 const superagent = require('superagent');
 const pg = require('pg');
+const methodOverride = require('method-override');
 const cors = require('cors');
 
+// Environment variables
+const PORT = process.env.PORT || 3030;
+const DATABASE_URL = process.env.DATABASE_URL;
 
 // Application Setup
 const app = express();
-const PORT = process.env.PORT || 3030;
-const DATABASE_URL = process.env.DATABASE_URL;
 app.use(cors());
 
-// Database Connection Setup
-const client = new pg.Client({
-    connectionString: DATABASE_URL,
-});
-
-
-
-// tell server that all stylesheets inside public 
+// Middleware
 app.use(express.static(__dirname + '/public'));
-
-// Application Middleware
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 // Set the view engine for server-side templating
 app.set('view engine', 'ejs');
 
-// API Routes
-// Renders the home page
+// Database Connection Setup
+const client = new pg.Client(process.env.DATABASE_URL);
+
+
+// Routes
 app.get('/', renderHomePage);
-
 app.get('/books/:id', viewBookDetails);
-
-app.post('/books', addBook)
-    // Renders the search form
 app.get('/searches/new', showForm);
-
-// Creates a new search to the Google Books API
+app.post('/books', addBook)
 app.post('/searches', createSearch);
+app.put('/books/:id', updateBook)
+app.delete('/books/:id', deleteBook)
 
-// Catch-all
-app.get('*', (request, response) => response.status(404).send('This route does not exist'));
-
-// app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
-
-// Constructor
+// Constructor function
 function Book(info) {
     this.image = info.imageLinks.smallThumbnail || 'https://i.imgur.com/J5LVHEL.jpeg';
     this.title = info.title || 'No title available';
     this.author = info.authors || ' ';
-    this.isbn = info.industryIdentifiers[0].identifier || ' '
+    this.isbn = info.industryIdentifiers[0].identifier || ' ';
     this.description = info.description || ' ';
 }
 
-// Note that .ejs file extension is not required
+
 function renderHomePage(request, response) {
     const sqlQuery = `SELECT * FROM books`;
-
-    // query the database
     client.query(sqlQuery).then(results => {
-
-        response.render('pages/index', { savedBooks: results.rows, booksNumber: results.rows.length })
+        response.render('pages/index', { savedBooks: results.rows, booksNumber: results.rows.length });
     });
 }
 
 function addBook(request, response) {
     const { title, author, isbn, image, description } = request.body;
     const safeValues = [title, author, isbn, image, description];
-    const bookObj = {
-        'image_url': image,
-        'title': title,
-        'author': author,
-        'isbn': isbn,
-        'description': description
-    }
-    const sqlQuery = 'INSERT INTO books (title, author, isbn, image_url, description) VALUES ($1, $2, $3, $4, $5);';
-    client.query(sqlQuery, safeValues).then((results) => {
-        response.render('pages/books/show', { bookDetails: bookObj });
-    }).catch(error => {
-        handleError(error, response);
+    const sqlQuery = 'INSERT INTO books (title, author, isbn, image_url, description) VALUES ($1, $2, $3, $4, $5) RETURNING id;';
+    client.query(sqlQuery, safeValues).then((result) => response.redirect(`/books/${result.rows[0].id}`));
+}
+
+function updateBook(request, response) {
+    const bookId = request.params.id;
+    const { title, author, isbn, image_url, description } = request.body;
+    const safeValues = [title, author, isbn, image_url, description, bookId];
+
+    const updateQuery = 'UPDATE books SET title=$1, author=$2, isbn=$3, image_url=$4, description=$5 WHERE id=$6;';
+
+    client.query(updateQuery, safeValues).then(results => {
+        response.redirect(`/books/${bookId}`);
+    });
+}
+
+function deleteBook(request, response) {
+    const bookId = request.params.id;
+    console.log(bookId);
+    const safeValues = [bookId];
+    const deleteQuery = 'DELETE FROM books WHERE id=$1';
+
+    client.query(deleteQuery, safeValues).then(() => {
+        response.redirect('/');
     });
 }
 
 function viewBookDetails(request, response) {
-
     const bookId = request.params.id;
     const safeValues = [bookId];
     const sqlSelectQuery = 'SELECT * FROM books WHERE id=$1';
-
     client.query(sqlSelectQuery, safeValues).then(results => {
         response.render('pages/books/show', { bookDetails: results.rows[0] });
     }).catch(error => {
@@ -103,14 +99,13 @@ function showForm(request, response) {
     response.render('pages/searches/new');
 }
 
-// No API key required
+
 function createSearch(request, response) {
     const url = 'https://www.googleapis.com/books/v1/volumes';
-
-    // add the search query to the URL
     const searchBy = request.body.searchBy;
     const searchValue = request.body.search;
     const queryObj = {};
+
     if (searchBy === 'title') {
         queryObj['q'] = `intitle:${searchValue}`;
 
@@ -118,7 +113,6 @@ function createSearch(request, response) {
         queryObj['q'] = `inauthor:${searchValue}`;
     }
 
-    // send the URL to the servers API
     superagent.get(url).query(queryObj).then(apiResponse => {
         return apiResponse.body.items.map(bookResult => {
             return new Book(bookResult.volumeInfo);
@@ -128,6 +122,10 @@ function createSearch(request, response) {
     }).catch((error) => {
         response.status(500).render('pages/error');
     });;
+}
+
+function handleError(error, response) {
+    response.render('pages/error-view', { error: error });
 }
 
 // Connect to DB and Start the Web Server
